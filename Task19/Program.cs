cb5477c44f3e04c0e1acb7723d8e54068b4bd23a
OlegKhudoerko <292oleg292@mail.ru>
2022-08-17 09:49:41 +0300

    decision 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -9,3 +9,18 @@
 // 23432 -> да
 
 
+Console.Write("Введите пятизначное число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+// int B = (A/10000)%10;
+// int C = (A/1000)%10;
+// int E = (A/10)%10;
+
+// int D = A%10;
+// Console.WriteLine(B);
+// Console.WriteLine(C);
+// Console.WriteLine(E);
+
+// Console.WriteLine(D);
+if ((A/10000)%10 == A%10 && (A/1000)%10 ==(A/10)%10) Console.WriteLine($"{A} -> да");
+else  Console.WriteLine($"{A} -> нет");
