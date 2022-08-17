a0001f09a7c69f388496e421e7aedfb5fd7e4169
OlegKhudoerko <292oleg292@mail.ru>
2022-08-17 08:48:59 +0300

    decision 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,3 +6,25 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+
+Console.Write("Введите координату точки А по оси Х:  ");
+int xA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки А по оси Y: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки А по оси Z: ");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси Х: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси Y: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси Z: ");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2));
+
+Console.WriteLine("Расстояние между точками A и B = " + Math.Round(distance, 2));
